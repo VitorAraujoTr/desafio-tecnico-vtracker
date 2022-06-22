0ffbada593b3d6f8e45420ab2d41c49025fcb4a6
Vitor Araújo <vitoraraujotr@gmail.com>
2022-06-22 18:48:25 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/vrtracker/vtracker/desafio/DesafioTecnicoVtrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.vrtracker.vtracker.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoVtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioTecnicoVtrackerApplication.class, args);
+    }
+
+}
